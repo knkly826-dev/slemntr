7a436cc94fb484c36ea510eb1638537725b61202
knkly826-dev <knkly826@gmail.com>
2026-09-02 12:36:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from gtts import gTTS
 
-TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")  # التوكن من Render
+TOKEN = os.environ.get("8824254463:AAFSitssn-jK0LKOtkoR1fWmIp-DmAgY-og")  # التوكن من Render
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🎙️ أرسل لي نصاً وسأحوله إلى صوت!")
